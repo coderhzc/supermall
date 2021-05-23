bd92e18b0ea12c0445cb18350b4328deb28d671c
Mac <mac@MacdeMacBook-Pro.local>
2021-05-23 22:52:48 +0800

    编辑管理

---
+++ src/components/common/TabBar Common/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tab-bal">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBal"
+}
+</script>
+
+<style scoped>
+.tab-bal{
+  display: flex;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background-color: rgba(227, 226, 226, 0.57);
+  box-shadow: 0px -4px 4px rgba(130, 128, 128,0.3);
+}
+
+</style>
---
+++ src/components/common/TabBar Common/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-bal-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="stylecolor"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  data(){
+    return{
+      // isActive: false,
+    }
+  },
+  props:{
+    path:{
+      type:String
+    },
+    activecolor:{
+      type:String,
+      default:'red',
+    }
+  },
+  methods:{
+    itemClick(){
+      return this.$router.push(this.path);
+    }
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    stylecolor(){
+      return this.isActive? {color:this.activecolor}:{}
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.tab-bal-item{
+  flex:1;
+  height: 49px;
+  text-align: center;
+}
+.tab-bal-item img{
+  height: 24px;
+  width: 24px;
+  vertical-align: middle;
+  margin-top: 3px;
+}
+</style>
